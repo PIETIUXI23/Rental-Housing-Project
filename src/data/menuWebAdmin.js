e71f1f21e184d64806eaeceb625dd4ca7a53b607
TheLez <152014804+TheLez@users.noreply.github.com>
2024-11-29 13:54:00 +0700

    update
    thêm menuWebAdmin

---
+++ src/data/menuWebAdmin.js
@@ -0,0 +1,38 @@
+export const MENU_WEBADMIN = [
+    {
+        id: 'home',
+        icon: 'fa-home',
+        label: 'Trang chính',
+        path: '/webadmin/home',
+    },
+    {
+        id: 'user-management',
+        icon: 'fa-users',
+        label: 'Quản lý người dùng',
+        path: '/webadmin/user-management',
+    },
+    {
+        id: 'advertisement-management',
+        icon: 'fa-file-alt',
+        label: 'Quản lý quảng cáo',
+        path: '/webadmin/advertisement-management',
+    },
+    {
+        id: 'service-packages',
+        icon: 'fa-history',
+        label: 'Quản lý gói dịch vụ',
+        path: '/webadmin/service-packages',
+    },
+    {
+        id: 'support',
+        icon: 'fa-life-ring',
+        label: 'Hỗ trợ',
+        path: '/webadmin/support',
+    },
+    {
+        id: 'settings',
+        icon: 'fa-cog',
+        label: 'Cài đặt',
+        path: '/webadmin/settings',
+    },
+];
